414af184d72a2e4750ef0c971742a8cb3e384755
Luis Gonzalez <luigonsa79@gmail.com>
2023-10-10 21:22:58 -0600

    add index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ejemplo</title>
+    </head>
+    <body>
+	<h1>hola desde el servidor</h1>
+
+        <?php
+            echo("¡Hola, soy un script de PHP!");
+        ?>
+        <h2>Hola mundo desde visual studio code</h2>
+
+    </body>
+</html>
