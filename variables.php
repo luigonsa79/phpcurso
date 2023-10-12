1acdcfe9d8f54a9ec6fbe08387c745c62c73b7b5
Luis Gonzalez <luigonsa79@gmail.com>
2023-10-11 20:08:06 -0600

    ejemplos variables

---
+++ variables.php
@@ -0,0 +1,20 @@
+<?php 
+// Una variable es esencial para almacenar y manipular datos en tus aplicaciones web
+// para empezar a declarar variables en php es tan simple como usar el signo $ o simbolo dolar y seguido el nombre de la variable
+
+$nombre = 'Luis gonzalez'; //aqui declaramos la variable, pero siempre tiene que tener un valor, el valor depende del tipo de datos, si es un texto o strign tiente que ir entre comillas, tambien una declaracion tiene que terminar en ; (punto y coma.), tambien se puede usar las comillas simpes ('') y es valido
+
+// en una variable se le puede asignar cualquier tipo de datos, mas adelante veremos los tipos de datos
+// las variable sno deben empezar con numeros, pero si pueden terminar con numeros
+
+// $7nombre = "lll"; nos da error al empezar con numero
+$edad1 = 32; //aqui si es valido aunque se uso numero al declarar la variable sin embargo esta esta al final. y el valor asignado es un numero entero, asi que no es necesario las comillas simpes ni dobles
+// mas adelante aprenderemos los ambitos de las variables, pues tambien encontramos variables globales en PHP.
+
+
+// Gracias por mirar y nos vemos en el proximo video de programacion en PHP👌
+
+
+
+
+
